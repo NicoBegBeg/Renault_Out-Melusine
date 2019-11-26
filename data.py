e5f8c04481bf29d0013825bc6b8716da251a1cb4
Liang019 <56045155+Liang019@users.noreply.github.com>
2019-11-26 10:47:39 +0100

    Update data.py

--- data.py
+++ data.py
@@ -8,6 +8,8 @@ areas=[Area("stock",500,{}),Area("transit",200,{}),Area("sortie_usine",0,{}),Are
 
 entries=[entry(12,"entree/sortie_CLE","nissan",15),entry(12,"entree/sortie_CLE","renault",16),entry(13,"entree/sortie_CLE","renault",15),entry(13,"entree/sortie_CLE","renault",17),entry(14,"entree/sortie_CLE","nissan",18)]
 outs=[out(12,14,"entree/sortie_CLE","nissan"),out(13,19,"entree/sortie_CLE","nissan"),out(13,13,"entree/sortie_CLE","renault"),out(13,14,"entree/sortie_CLE","renault"),out(13,15,"entree/sortie_CLE","renault")] #compléter avec constraints
+
 inaterliers=[inatelier(12,"stock","atelier","nissan"),inatelier(13,"stock","atelier","nissan"),inatelier(12,"stock","atelier","renault"),inatelier(12,"transit","atelier","renault"),inatelier(13,"transit","atelier","renault")]
-outateliers=[outatelier(12,"transit","atelier","nissan"),outatelier(12,"transit","atelier","nissan")
+outateliers=[outatelier(13,"stock","atelier","nissan"),outatelier(14,"stock","atelier","nissan"),outatelier(15,"stock","atelier","renault"),outatelier(15,"transit","atelier","renault"),outatelier(16,"transit","atelier","renault")]
+
 CLE_ESSAI_1=CLE(areas,dictionnaire_input,entry,out)
