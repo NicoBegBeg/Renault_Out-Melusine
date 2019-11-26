b51b089b53a6320572be219f6ca8f0427ca0ce23
unknown <nicolas.begaud@hotmail.fr>
2019-11-26 10:00:29 +0100

    ajouts

--- CLE_class.py
+++ CLE_class.py
@@ -1,3 +1,6 @@
+import Task_class
+import Area_class
+
 class CLE:
     
     def __init__(self,areas,temps_vecteur,entry,out):
@@ -5,4 +8,10 @@ class CLE:
         self.temps_vecteur=temps_vecteur
         self.entry=entry
         self.out=out
-        
+    
+    def apply_task(self,task):
+        D=self.areas[task.d_area]
+        A=self.areas[task.a_area]
+        if max(D.can_remove_car(task.car_type,1),A.can_add_car(task.car_type,1)):
+            D.remove_car(task.car_type,1)
+            A.add_car(task.car_type,1)
