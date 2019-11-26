9ac4531608f47c49e02a8244c2c03863f6a5bda6
unknown <nicolas.begaud@hotmail.fr>
2019-11-26 10:17:30 +0100

    tentative d'initialisation

--- data.py
+++ data.py
@@ -1,4 +1,11 @@
 #Fichier contenant toutes les données du problème
 
-import numpy as np
+import temps_vecteur
+import contraints
 
+areas=[Area("stock",500,{}),Area("transit",200,{}),Area("sortie_usine",0,{}),Area("atelier",100,{}),Area("entree/sortie_CLE",100,{})]
+
+entry=[]
+out=[] #compléter avec constraints
+
+CLE_ESSAI_1=CLE(areas,dictionnaire_input,entry,out)
