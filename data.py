baf16ccab45f22c7db10991e10ef0c70f26e5989
unknown <nicolas.begaud@hotmail.fr>
2019-11-26 10:20:37 +0100

    ajouts

--- data.py
+++ data.py
@@ -3,9 +3,13 @@
 import temps_vecteur
 import contraints
 
-areas=[Area("stock",500,{}),Area("transit",200,{}),Area("sortie_usine",0,{}),Area("atelier",100,{}),Area("entree/sortie_CLE",100,{})]
+areas=[Area("stock",500,{}),Area("transit",200,{}),Area("sortie_usine",0,{"clio":3, "zoé":50}),Area("atelier",100,{}),Area("entree/sortie_CLE",100,{})]
 
 entry=[]
 out=[] #compléter avec constraints
 
 CLE_ESSAI_1=CLE(areas,dictionnaire_input,entry,out)
+
+#Test
+
+Task("sortie_usine","stock","clio",10)
