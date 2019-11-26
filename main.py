d5cab3d8275bba8903cfad06fd15cfc5f3c07b7d
Luca SPANTI <luca.spanti@eleves.enpc.fr>
2019-11-26 11:05:00 +0100

    add precision in main commentaries

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ from data import *
 #On somme enfin les durées de toutes les tâches pour obtenir un temps total de travail pour une journée
 #A partir de ce temps total, on en déduit combien de jockeys sont nécessaires à faire fonctionner le CLE
 
+#Pour l'instant, les contrantes de temps sont ignorées
+#Il faudra aussi ajouter les contraintes liées aux Jockeys (disponibilité/ils se déplacent par 3 + 1 navette)
+
 if __name__ == "__main__":
     None
