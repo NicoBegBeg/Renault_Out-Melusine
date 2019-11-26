e7b445f76c7a1e746622b7924bff274d24e841fe
Luca SPANTI <luca.spanti@eleves.enpc.fr>
2019-11-26 10:56:23 +0100

    add commentaries

--- CLE_class.py
+++ CLE_class.py
@@ -12,6 +12,9 @@ class CLE:
     def apply_task(self,task):
         D=self.areas[task.d_area]
         F=self.areas[task.f_area]
+
+        # Plutot mettre des raiseError
+
         if max(D.can_remove_car(task.car_type,1),F.can_add_car(task.car_type,1)):
             D.remove_car(task.car_type,1)
             F.add_car(task.car_type,1)
