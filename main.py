c78320d32b91bf343a475b35d1e9a9c40a8ddf7d
Luca SPANTI <luca.spanti@eleves.enpc.fr>
2019-11-26 10:22:52 +0100

    wrote goal for main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import Task_class, CLE_class, Area_class, contraints, temps_vecteur, data
 
+#Principe : A partir des contraintes, on effectue une série de tâches pour y répondre -> chaque tâche demande un temps t
+#Sans tenir compte de limites temporelles/matérielles/humaines, on applique toutes ces tâches successives au CLE
+#On somme enfin les durées de toutes les tâches pour obtenir un temps total de travail pour une journée
+#A partir de ce temps total, on en déduit combien de jockeys sont nécessaires à faire fonctionner le CLE
+
 if __name__ == "__main__":
     None
