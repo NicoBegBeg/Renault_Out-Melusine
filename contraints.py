25f5d69d3367c9c085f32368a77e6fa0d8a43240
Luca SPANTI <luca.spanti@eleves.enpc.fr>
2019-11-26 10:49:19 +0100

    fix constraints inits

--- contraints.py
+++ contraints.py
@@ -1,26 +1,26 @@
 class entry:
-    def _init_(self,entry_time,entry_area,model,out_date):
+    def __init__(self,entry_time,entry_area,model,out_date):
         self.entry_time = entry_time
         self.entry_area = entry_area
         self.model = model
         self.out_date = out_date
 
 class out:
-    def _init_(self,outtime_min,outtime_max,out_area,model):
+    def __init__(self,outtime_min,outtime_max,out_area,model):
         self.outtime_min = outtime_min
         self.outtime_max = outtime_max
         self.out_area = out_area
         self.model = model
 
 class inatelier:
-    def _init_(self,entry_time,area_in,area_atelier,model):
+    def __init__(self,entry_time,area_in,area_atelier,model):
         self.entry_time = entry_time
         self.area_in = area_in
         self.area_atelier = area_atelier
         self.model = model
 
 class outatelier:
-    def _init_(self,out_time,area_atelier,area_out,model):
+    def __init__(self,out_time,area_atelier,area_out,model):
         self.out_time = out_time
         self.area_atelier = area_atelier
         self.area_out = area_out
