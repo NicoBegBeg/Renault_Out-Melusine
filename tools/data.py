28d288532322495db6f3914403e3bff83162400a
Luca SPANTI <luca.spanti@eleves.enpc.fr>
2019-12-10 10:10:30 +0100

    edit data

--- tools/data.py
+++ tools/data.py
@@ -2,8 +2,9 @@
 
 from tools.constraints import *
 from tools.CLE_class import *
+import random as rng
 
-areas={"stock":Area("stock",500,{}),"transit":Area("transit",200,{}),"sortie_usine":Area("sortie_usine",0,{}),"atelier":Area("atelier",100,{}),"entree/sortie_CLE":Area("entree/sortie_CLE",100,{})}
+areas={"stock":Area("stock",500,{}),"transit":Area("transit",200,{}),"sortie_usine":Area("sortie_usine",100,{}),"atelier":Area("atelier",100,{}),"entree/sortie_CLE":Area("entree/sortie_CLE",100,{})}
 
 entries_1=[entry(12,"sortie_usine","nissan",15),entry(12,"sortie_usine","renault",16),entry(13,"sortie_usine","renault",15),entry(13,"sortie_usine","renault",17),entry(14,"sortie_usine","nissan",18)]
 
@@ -12,5 +13,32 @@ outs_1=[out(12,14,"entree/sortie_CLE","nissan"),out(13,19,"entree/sortie_CLE","n
 in_ateliers_1=[in_atelier(12,"stock","atelier","nissan"),in_atelier(13,"stock","atelier","nissan"),in_atelier(12,"stock","atelier","renault"),in_atelier(12,"transit","atelier","renault"),in_atelier(13,"transit","atelier","renault")]
 
 out_ateliers_1=[out_atelier(13,"stock","atelier","nissan"),out_atelier(14,"stock","atelier","nissan"),out_atelier(15,"stock","atelier","renault"),out_atelier(15,"transit","atelier","renault"),out_atelier(16,"transit","atelier","renault")]
+
+
+
+total_minutes = 60 * 12
+
+entries_2=[]
+outs_2=[]
+in_ateliers_2=[]
+out_ateliers_2=[]
+
+for minute in total_minutes:
+    if rng.random() >= 0.5:
+        continue
+    if rng.random() >= 0.5:
+        modele = "renault"
+    else:
+        modele = "nissan"
+    if rng.random() >= 0.5:
+        if rng.random() >= 0.5:
+            entries_2 += [entry(12,"sortie_usine",modele,minute)]
+        else:
+            out_2 += [out(minute,minute+15,"entree/sortie_CLE",modele)]
+    else:
+        if rng.random() >= 0.5:
+            in_ateliers_2 += [in_atelier(minute,"stock","atelier",modele)]
+        else:
+            out_ateliers_2=[out_atelier(minute,"stock","atelier",modele)]
 
 CLE_ESSAI_1=CLE(areas,dictionnaire_input,entry,out,['sortie_usine'],['entree/sortie_CLE'])
